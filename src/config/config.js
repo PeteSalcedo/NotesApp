619c138ec3bd0dd93ab90a83722f9731b5930934
PeteSalcedo <PEter.salcedo.diaz@gmail.com>
2018-10-18 19:49:28 -0500

    Uploading the note app

---
+++ src/config/config.js
@@ -0,0 +1,8 @@
+export const DB_config = {
+    apiKey: "AIzaSyBg3R2MVEUtd84t70o6GZStzTOHGB6Rsis",
+    authDomain: "noteapp-bd9bc.firebaseapp.com",
+    databaseURL: "https://noteapp-bd9bc.firebaseio.com",
+    projectId: "noteapp-bd9bc",
+    storageBucket: "noteapp-bd9bc.appspot.com",
+    messagingSenderId: "582717137185"
+  };
